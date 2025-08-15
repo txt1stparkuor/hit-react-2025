a2d1467fe6bc7bd2fd84d7232b19dc77b16499ce
txt1stparkuor <txt1stparkuor@gmail.com>
2025-08-15 08:58:02 +0700

    123

---
+++ src/store/slice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  count: 0,
+};
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    incrementCount: (state) => {
+      state.count += 1;
+    },
+    decrementCount: (state) => {
+      state.count -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.count += action.payload;
+    },
+  },
+});
+
+export const { incrementCount, decrementCount, incrementByAmount } = counterSlice.actions;
+
+export default counterSlice.reducer;
